784f7d340dd9a111d165074d5ad9e5fb23fe3d3b
DODI ZEPANYA MANURUNG <149552320+DODIZEPANYA@users.noreply.github.com>
2023-11-08 12:39:13 +0700

    Add files via upload

--- Nomor1.js
+++ Nomor1.js
@@ -1,5 +1,11 @@
-function displayMessage() {
-  console.log("Hello World");
+function minimal(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
 }
 
-displayMessage();
+console.log(minimal(1, 4)); // Output: 1
+console.log(minimal(3, 2)); // Output: 2
+console.log(minimal(3, 3)); // Output: 3
